18d8582dc8f140c043a62728762d937801caddf3
Vegard Falldalen <vegfall@gmail.com>
2023-10-24 12:39:02 +0200

    Added fraction testing in main

--- FractionApp/Program.cs
+++ FractionApp/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Fraction fraction1 = new(5, 10);
+            Fraction fraction2 = new(10, 20);
+
+            Console.WriteLine($"5|10 * 10|20 = {fraction1 * fraction2}");
+            Console.WriteLine($"5|10 / 10|20 = {fraction1 / fraction2}");
+            Console.WriteLine($"5|10 + 10|20 = {fraction1 + fraction2}");
+            Console.WriteLine($"5|10 - 10|20 = {fraction1 - fraction2}");
+            Console.WriteLine($"5|10 == 10|20 = {fraction1.Equals(fraction2)}");
 
             Console.ReadKey();
         }
