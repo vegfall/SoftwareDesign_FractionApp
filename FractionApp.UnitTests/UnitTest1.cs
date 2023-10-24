82dba98f439d85ded39038bd55de1dd9dd54c93c
Vegard Falldalen <vegfall@gmail.com>
2023-10-24 12:41:49 +0200

    Added nunit to project

---
+++ FractionApp.UnitTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace FractionApp.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+            Assert.IsTrue(true);
+        }
+    }
+}
